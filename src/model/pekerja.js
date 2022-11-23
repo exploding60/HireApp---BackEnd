c1ffe22bb0e63eb7a266d493953019d1ce9f94df
exploding60 <rizkysyahputra14045@gmail.com>
2022-11-24 00:19:50 +0700

    benerin kodingan haiya

--- src/model/pekerja.js
+++ src/model/pekerja.js
@@ -1,4 +1,4 @@
-const Pool = require('./../config/db')
+const Pool = require("./../config/db");
 
 // const insertData = (data) => {
 //     const {id,name,password,email,phonenumber,profile,skill,auth,image,idcompany} = data;
@@ -6,9 +6,11 @@ const Pool = require('./../config/db')
 // }
 
 const insertData = (data) => {
-    const {id,name,password,email,phonenumber,profile,skill,auth,image,idcompany} = data;
-    return Pool.query(`INSERT INTO pekerja(id,name,password,email,phonenumber,profile,skill,auth,image,idcompany) VALUES(${id},'${name}','${password}','${email}',${phonenumber},'${profile}','${skill}',${auth},'${image}',${idcompany})`)
-}
+  const { id, name, password, email, phonenumber, auth } = data;
+  return Pool.query(
+    `INSERT INTO pekerja(id,name,password,email,phonenumber,auth) VALUES(${id},'${name}','${password}','${email}',${phonenumber},${auth})`
+  );
+};
 
 // const insertData = (data) => {
 //     const {id,name,password,email,phonenumber,auth,} = data;
@@ -16,6 +18,6 @@ const insertData = (data) => {
 // }
 
 const getPekerja = () => {
-    return Pool.query(`SELECT * FROM pekerja`)
-}
-module.exports = {insertData,getPekerja}
+  return Pool.query(`SELECT * FROM pekerja`);
+};
+module.exports = { insertData, getPekerja };
--- src/routes/pekerja.js
+++ src/routes/pekerja.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const router = express.Router()
-const {PekerjaController} = require('../controller/pekerja')
+const express = require("express");
+const router = express.Router();
+const { PekerjaController } = require("../controller/pekerja");
 
-router.post ('/',PekerjaController.insert)
-router.get ('/',PekerjaController.getPekerja)
+router.post("/", PekerjaController.insert);
+// router.get ('/',PekerjaController.getPekerja)
 
-module.exports = router
+module.exports = router;
