35da09db165b62deefc54343ba05c06e2bf653f5
RizRev <rizkyrevanda@gmail.com>
2022-11-25 09:57:16 +0700

    confirm password

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
--- Active: 1669184842080@@satao.db.elephantsql.com@5432@apfpynxx@public
+-- Active: 1669197341670@@satao.db.elephantsql.com@5432@apfpynxx
 CREATE TABLE work_experiences (
     id_work INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
     role VARCHAR (100) NOT NULL,
@@ -7,3 +7,5 @@ CREATE TABLE work_experiences (
     id_pekerja VARCHAR (100),
     created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
+
+ALTER TABLE perekrut ALTER COLUMN id TYPE VARCHAR;
