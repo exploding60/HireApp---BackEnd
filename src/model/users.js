2f4c7d9c953102c15f3b05b8e36506b31b2ef090
exploding60 <rizkysyahputra14045@gmail.com>
2022-11-28 12:04:45 +0700

    edit Select data users

--- src/model/users.js
+++ src/model/users.js
@@ -93,12 +93,12 @@ const updateUsersPekerja = (id, data) => {
 };
 const selectDataPekerja = (id) => {
   return Pool.query(
-    `SELECT users.id,users.name,users.email,users.deskripsi,users.domisili,users.tempat_kerja FROM users WHERE users.id='${id}'`
+    `SELECT users.id,users.name,users.email,users.deskripsi,users.domisili,users.tempat_kerja,users.ig,users.linkedin FROM users WHERE users.id='${id}'`
   );
 };
 const selectDataPerekrut = (id) => {
   return Pool.query(
-    `SELECT users.id,users.company_name,users.email_perusahaan,users.deskripsi,users.domisili,users.bidang,users.linkedin,users.ig FROM users WHERE users.id='${id}'`
+    `SELECT users.id,users.company_name,users.email_perusahaan,users.deskripsi,users.phonenumber,users.domisili,users.bidang,users.linkedin,users.ig FROM users WHERE users.id='${id}'`
   );
 };
 const getHomePekerja = () => {
@@ -106,9 +106,13 @@ const getHomePekerja = () => {
     `SELECT id,name,bidang,domisili,image FROM users WHERE role='pekerja'`
   );
 };
-const getSortPekerja = (sort,page,limit,search) => {
-  return Pool.query(`SELECT id,name,bidang,domisili,role FROM users WHERE role='pekerja' AND name ILIKE '%${search}%' ORDER BY name ${sort} limit ${limit} offset ${(page-1)*limit}`);
-}
+const getSortPekerja = (sort, page, limit, search) => {
+  return Pool.query(
+    `SELECT id,name,bidang,domisili,role FROM users WHERE role='pekerja' AND name ILIKE '%${search}%' ORDER BY name ${sort} limit ${limit} offset ${
+      (page - 1) * limit
+    }`
+  );
+};
 module.exports = {
   createPerekrut,
   createPekerja,
@@ -120,5 +124,5 @@ module.exports = {
   selectDataPekerja,
   selectDataPerekrut,
   getHomePekerja,
-  getSortPekerja
+  getSortPekerja,
 };
